76e6b04ec74c283eec7ed6f992f7c2e3e5e3f151
anish-singh-rawat <anishsinghrawat1>
2024-06-06 14:26:00 +0530

    worked adding new API URL

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -33,7 +33,7 @@ const Footer = () => {
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-800">
-          © 2023{" "}
+          © 2024{" "}
           <Link to={'/'} className="hover:underline">
             Ecommerce™
           </Link>
